f68cf34cefff65e8b299f486fb8bde3f146351f7
cookcodeblog <cookcode_me@sina.com>
2018-06-06 23:55:35 +0800

    Yum install kubelet, kubeadm and kubectl together

--- kubeadm/03_install_kubenetes.sh
+++ kubeadm/03_install_kubenetes.sh
@@ -2,9 +2,7 @@
 ./use_aliyun_kubernetes_yum_source.sh
 
 setenforce 0
-yum install -y kubelet-1.10.3
-yum install -y kubeadm-1.10.3
-yum install -y kubectl-1.10.3
+yum install -y kubelet-1.10.3 kubeadm-1.10.3 kubectl-1.10.3
 systemctl enable kubelet && systemctl start kubelet
 
 # Configure cgroup matched with Docker
