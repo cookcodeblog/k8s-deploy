ed29e9c3c898c16666f5878761d5e847494355cc
cookcodeblog <cookcode_me@sina.com>
2018-06-06 23:31:48 +0800

    Add sleep time before get pods

--- kubeadm/06_install_flannel.sh
+++ kubeadm/06_install_flannel.sh
@@ -3,5 +3,7 @@
 wget https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
 kubectl apply -f kube-flannel.yml
 
+# Wait a while to let network takes effect
+sleep 10
 kubectl get pods --all-namespaces
 
