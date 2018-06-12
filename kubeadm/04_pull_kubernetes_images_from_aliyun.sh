a7c834a99cf47988ade6c1abfa58ccb16af39a2a
cookcodeblog <cookcode_me@sina.com>
2018-06-12 22:50:51 +0800

    Update pull script

--- kubeadm/04_pull_kubernetes_images_from_aliyun.sh
+++ kubeadm/04_pull_kubernetes_images_from_aliyun.sh
@@ -7,13 +7,21 @@ set -e
 # For running kubeadm without an internet connection you have to pre-pull the required master images for the version of choice:
 KUBE_VERSION=v1.10.3
 KUBE_PAUSE_VERSION=3.1
+ETCD_VERSION=3.1.12
+DNS_VERSION=1.14.8
 
 GCR_URL=k8s.gcr.io
 ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cookcodeblog
 
 images=(kube-proxy-amd64:${KUBE_VERSION}
+kube-scheduler-amd64:${KUBE_VERSION}
+kube-controller-manager-amd64:${KUBE_VERSION}
+kube-apiserver-amd64:${KUBE_VERSION}
 pause-amd64:${KUBE_PAUSE_VERSION}
-)
+etcd-amd64:${ETCD_VERSION}
+k8s-dns-sidecar-amd64:${DNS_VERSION}
+k8s-dns-kube-dns-amd64:${DNS_VERSION}
+k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION})
 
 
 for imageName in ${images[@]} ; do
