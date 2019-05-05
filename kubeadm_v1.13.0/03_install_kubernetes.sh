2b1505e7af34f260cf2e51f9f39feca388c754b5
cookcodeblog <cookcode_me@sina.com>
2019-05-05 16:52:10 +0800

    Add kubeadm install script for v1.13

--- kubeadm_v1.13.0/03_install_kubernetes.sh
+++ kubeadm_v1.13.0/03_install_kubernetes.sh
@@ -5,7 +5,10 @@ set -e
 ./use_aliyun_kubernetes_yum_source.sh
 
 setenforce 0
-yum install -y kubelet-1.13.0 kubeadm-1.13.0 kubectl-1.13.0
+yum install -y kubelet-1.13.0
+yum install -y kubeadm-1.13.0
+yum install -y kubectl-1.13.0
+
 systemctl enable kubelet && systemctl start kubelet
 
 # Configure cgroup matched with Docker
