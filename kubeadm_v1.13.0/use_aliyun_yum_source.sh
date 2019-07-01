210bda8ac672d3c563b022551ec218cda8c6088d
cookcodeblog <williamsrlin@outlook.com>
2019-07-01 14:37:18 +0800

    Add Aliyun EPEL 7 yum source

--- kubeadm_v1.13.0/use_aliyun_yum_source.sh
+++ kubeadm_v1.13.0/use_aliyun_yum_source.sh
@@ -4,6 +4,8 @@ set -e
 
 #wget -O /etc/yum.repos.d/CentOS7-Aliyun.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 cp -p ./CentOS7-Aliyun.repo /etc/yum.repos.d
+cp -p ./epel-7-Aliyun.repo /etc/yum.repos.d
+
 yum clean all
 yum makecache -y
 yum repolist all
