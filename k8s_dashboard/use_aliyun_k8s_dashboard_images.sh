d3feeacfa1ec9a34ec7284886627f50dce6c19c6
cookcodeblog <williamsrlin@outlook.com>
2019-07-01 17:15:22 +0800

    Modify dashbaord path with v1.8.3 version

--- k8s_dashboard/use_aliyun_k8s_dashboard_images.sh
+++ k8s_dashboard/use_aliyun_k8s_dashboard_images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget -O kubernetes-dashboard.yaml https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+wget -O kubernetes-dashboard.yaml https://raw.githubusercontent.com/kubernetes/dashboard/v1.8.3/src/deploy/recommended/kubernetes-dashboard.yaml
 cp -p kubernetes-dashboard.yaml kubernetes-dashboard.yaml.bak$(date '+%Y%m%d%H%M%S')
 
 # Replace k8s.gcr.io image with registry.cn-shenzhen.aliyuncs.com/cookcodeblog
