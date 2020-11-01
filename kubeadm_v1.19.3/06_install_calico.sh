84e77056197acf3aac6060e81c16f3becdc41a79
cookcodeblog <cookcode_me@sina.com>
2020-11-01 11:24:53 +0800

    calico support different iface

--- kubeadm_v1.19.3/06_install_calico.sh
+++ kubeadm_v1.19.3/06_install_calico.sh
@@ -4,8 +4,16 @@ set -e
 
 
 # wget -O ./calico/calico.yaml https://docs.projectcalico.org/v3.16/manifests/calico.yaml
-kubectl apply -f ./calico/calico.yaml
+IFACE="$1"
+MANIFEST_NAME="calico.yaml"
+if [[ -n "${IFACE}" ]]; then
+  MANIFEST_NAME="calico-${IFACE}.yaml"
+fi
+
+kubectl apply -f ./calico/${MANIFEST_NAME}
 
 # Wait a while to let network takes effect
 sleep 30
-./k8s_health_check.sh
+
+kubectl get pods -n=kube-system -l k8s-app=calico-node
+
